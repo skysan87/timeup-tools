2793be089380ae3f6288614b9ce8cc510921aeaa
skysan <26142873+skysan87@users.noreply.github.com>
2023-10-09 21:54:33 +0900

    タスクの繰り返しテスト追加

--- packages/core/test/Domain/Behavior/HabitBehavior.test.ts
+++ packages/core/test/Domain/Behavior/HabitBehavior.test.ts
@@ -1,6 +1,5 @@
-import { dateFactory } from "@/Util/DateUtil"
 import { Habit } from "@/Domain/Model/Habit"
-import { Frequnecy, FullYear, Weekdays } from "@/Domain/ValueObject"
+import { Frequnecy, FullYear, Weekdays, MonthlyType } from "@/Domain/ValueObject"
 import { HabitBehavior } from "@/Domain/Behavior/HabitBehavior"
 import { IBehavior } from "@/Domain/Behavior/IBehavior"
 
@@ -41,7 +40,7 @@ describe('main', () => {
     const year = today.getFullYear()
     const month = today.getMonth()
 
-    // - 今日の実施予定日が`1`になっていること
+    // 今日の実施予定日が`1`になっていること
     // 16進数の値 一日なら 1100....0(32文字) -> c0000000(8文字)
     const thisMonthPlan = result.plan[year as FullYear][month]
     expect(thisMonthPlan).toBe('c0000000')
@@ -71,11 +70,132 @@ describe('main', () => {
     const year = today.getFullYear()
     const month = today.getMonth() // 8 = 9月
 
-    // - 9月分の実施予定日が計算されていること
+    // 9月分の実施予定日が計算されていること
     const lastMonthPlan = result.plan[year as FullYear][month]
     expect(lastMonthPlan).toBe('c183060c')
 
     // 2022/10/1は土曜日なので対象日でない
     expect(result.isPlanDay).toBe(false)
   })
+
+  test('毎月1日のタスクで10/1が設定されること', () => {
+    // 最終更新日(summaryUpdatedAt)が8/31で10/1に更新
+    // 9/1-10/1までの更新の実施予定日を更新する
+    const data: Habit = {
+      id: '',
+      rootId: '',
+      title: '毎月1日のタスク',
+      frequency: Frequnecy.MONTHLY,
+      monthlyType: MonthlyType.DAY,
+      planDays: [1],
+      summaryUpdatedAt: 20220831
+    } as Habit
+
+    const result: Habit = new HabitBehavior(data).action((behavior: IBehavior<Habit>) => {
+      const b = behavior as HabitBehavior
+      b.updateSummary()
+    })
+
+    const today = new Date(2022, 9, 1) // 2022/10/01
+    const year = today.getFullYear()
+    const month = today.getMonth()
+
+    // - 10月の実施予定日が計算されていること
+    const thisMonthPlan = result.plan[year as FullYear][month]
+    expect(thisMonthPlan).toBe('c0000000')
+
+    // 2022/10/1は対象日
+    expect(result.isPlanDay).toBe(true)
+  })
+
+  test('毎月31日のタスクで9月分が設定されないこと', () => {
+    // NOTE: 9月31日はない
+    // 最終更新日(summaryUpdatedAt)が8/31で10/1に更新
+    // 9/1-10/1までの更新の実施予定日を更新する
+    const data: Habit = {
+      id: '',
+      rootId: '',
+      title: '毎月31日のタスク',
+      frequency: Frequnecy.MONTHLY,
+      monthlyType: MonthlyType.DAY,
+      planDays: [31],
+      summaryUpdatedAt: 20220831
+    } as Habit
+
+    const result: Habit = new HabitBehavior(data).action((behavior: IBehavior<Habit>) => {
+      const b = behavior as HabitBehavior
+      b.updateSummary()
+    })
+
+    const today = new Date(2022, 8, 1) // 2022/9/01
+    const year = today.getFullYear()
+    const month = today.getMonth()
+
+    // - 9月の実施予定日が計算されていること
+    const lastMonthPlan = result.plan[year as FullYear][month]
+    expect(lastMonthPlan).toBe('80000000')
+
+    // 2022/10/1は対象日でない
+    expect(result.isPlanDay).toBe(false)
+  })
+
+  test('毎月月末のタスクで9/30が設定されること', () => {
+    // 最終更新日(summaryUpdatedAt)が8/31で10/1に更新
+    // 9/1-10/1までの更新の実施予定日を更新する
+    const data: Habit = {
+      id: '',
+      rootId: '',
+      title: '月末のタスク',
+      frequency: Frequnecy.MONTHLY,
+      monthlyType: MonthlyType.END,
+      summaryUpdatedAt: 20220831
+    } as Habit
+
+    const result: Habit = new HabitBehavior(data).action((behavior: IBehavior<Habit>) => {
+      const b = behavior as HabitBehavior
+      b.updateSummary()
+    })
+
+    const today = new Date(2022, 8, 1) // 2022/9/01
+    const year = today.getFullYear()
+    const month = today.getMonth()
+
+    // 9月の実施予定日が計算されていること
+    const lastMonthPlan = result.plan[year as FullYear][month]
+    expect(lastMonthPlan).toBe('80000002')
+
+    // 2022/10/1は対象日でない
+    expect(result.isPlanDay).toBe(false)
+  })
+
+  test('毎月第一土曜日のタスクで10/1が設定されること', () => {
+    // 最終更新日(summaryUpdatedAt)が8/31で10/1に更新
+    // 9/1-10/1までの更新の実施予定日を更新する
+    const data: Habit = {
+      id: '',
+      rootId: '',
+      title: '第一土曜日のタスク',
+      frequency: Frequnecy.MONTHLY,
+      monthlyType: MonthlyType.WEEK,
+      planWeek: { index: 1, day: 6 }, // TODO: 曜日を定義
+      summaryUpdatedAt: 20220831
+    } as Habit
+
+    const result: Habit = new HabitBehavior(data).action((behavior: IBehavior<Habit>) => {
+      const b = behavior as HabitBehavior
+      b.updateSummary()
+    })
+
+    const today = new Date(2022, 9, 1) // 2022/10/01
+    const year = today.getFullYear()
+    const month = today.getMonth()
+
+    // 10月の実施予定日が計算されていること
+    const thisMonthPlan = result.plan[year as FullYear][month]
+    expect(thisMonthPlan).toBe('c0000000')
+
+    // 2022/10/1は対象日
+    expect(result.isPlanDay).toBe(true)
+  })
+
 })
