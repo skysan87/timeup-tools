962ffcbf36fbccd9254bde2a4444eb256854b289
skysan <26142873+skysan87@users.noreply.github.com>
2024-02-14 21:47:53 +0900

    TaskRepository実装

---
+++ packages/firebase-infra/src/Repository/TaskRepository.ts
@@ -0,0 +1,227 @@
+import { CollectionReference, DocumentData, DocumentSnapshot, Query, collection, doc, getCountFromServer, getDocs, query, serverTimestamp, where } from "firebase/firestore"
+import { Task } from "@timeup-tools/core/model"
+import { ITaskRepository } from "@timeup-tools/core/repository"
+import { UserId, DateNumber, TaskType, TaskState } from "@timeup-tools/core/value-object"
+import { firestore } from "../AppSetting"
+import { scope } from "./Transaction"
+import { toTaskEntity } from "@/Converter"
+import { getCount } from "firebase/firestore/lite"
+
+export class TaskRepository implements ITaskRepository {
+
+  private static readonly MAX_COUNT: number = 100
+
+  private getRef(userId: UserId): CollectionReference {
+    // TODO: 将来的に構造を変更する(userIdを利用)
+    return collection(firestore, 'todos')
+  }
+
+  public async validateMaxSize(userId?: UserId, tasklistId?: string): Promise<boolean> {
+    const q = query(this.getRef(userId!)
+      , where('listId', '==', tasklistId!)
+      , where('userId', '==', userId!)
+    )
+
+    // TIPS:
+    //  1000件で1Read
+    //  https://cloud.google.com/firestore/pricing?hl=ja#aggregation_queries
+    let count: number
+    try {
+      const snapshot = await getCountFromServer(q)
+      count = snapshot.data().count
+    } catch (err) {
+      console.warn('Server Access Failed.', err)
+      const snapshot = await getCount(q)
+      count = snapshot.data().count
+    }
+    return count < TaskRepository.MAX_COUNT
+  }
+
+  public async getHabits(userId: UserId, today: DateNumber): Promise<Task[]> {
+    const tasks: Task[] = []
+
+    const q = query(this.getRef(userId)
+      , where('type', '==', TaskType.HABIT)
+      , where('userId', '==', userId)
+      , where('startdate', '==', today)
+    )
+
+    tasks.push(...await this.getRecords(q))
+
+    return tasks
+  }
+
+  public async getTodaysTasks(userId: UserId, today: DateNumber): Promise<Task[]> {
+    const tasks: Task[] = []
+
+    const q = (state: TaskState) => {
+      return query(this.getRef(userId)
+        , where('type', '==', TaskType.TODO)
+        , where('userId', '==', userId)
+        , where('state', '==', state)
+        , where('startdate', '<=', today)
+      )
+    }
+
+    tasks.push(...await this.getRecords(q(TaskState.Todo)))
+    tasks.push(...await this.getRecords(q(TaskState.InProgress)))
+
+    return tasks
+  }
+
+  public async getInProgressTasks(userId: UserId, today: DateNumber): Promise<Task[]> {
+    const tasks: Task[] = []
+
+    const qTodo = query(this.getRef(userId)
+      , where('type', '==', TaskType.TODO)
+      , where('userId', '==', userId)
+      , where('state', '==', TaskState.InProgress)
+    )
+
+    tasks.push(...await this.getRecords(qTodo))
+
+    const qHabit = query(this.getRef(userId)
+      , where('type', '==', TaskType.HABIT)
+      , where('userId', '==', userId)
+      , where('startdate', '==', today)
+      , where('state', '==', TaskState.InProgress)
+    )
+
+    tasks.push(...await this.getRecords(qHabit))
+
+    return tasks
+  }
+
+  public async getTodaysDone(userId: UserId, today: DateNumber): Promise<Task[]> {
+    const tasks: Task[] = []
+
+    const q = query(this.getRef(userId)
+      , where('type', '==', TaskType.TODO)
+      , where('userId', '==', userId)
+      , where('state', '==', TaskState.Done)
+      , where('stateChangeDate', '==', today)
+    )
+
+    tasks.push(...await this.getRecords(q))
+
+    return tasks
+  }
+
+  public async get(userId: UserId, tasklistId: string): Promise<Task[]> {
+    const tasks: Task[] = []
+
+    const q = query(this.getRef(userId)
+      , where('listId', '==', tasklistId)
+      , where('userId', '==', userId)
+    )
+
+    tasks.push(...await this.getRecords(q))
+
+    return tasks
+  }
+
+  public async getById(userId: UserId, taskId: string): Promise<Task | null> {
+    const docRef = doc(this.getRef(userId), taskId)
+    const snapshot: DocumentSnapshot = await scope.get(docRef)
+
+    if (!snapshot.exists()) {
+      return null
+    }
+
+    return this.convert(snapshot.id, snapshot.data())
+  }
+
+  public async save(userId: UserId, data: Task): Promise<Task> {
+    const entity = toTaskEntity(data)
+    entity.createdAt = serverTimestamp()
+    entity.updatedAt = serverTimestamp()
+
+    const newDocRef = doc(this.getRef(userId))
+    await scope.set(newDocRef, entity)
+
+    const systemDate = new Date()
+    const newData = structuredClone(data)
+    newData.id = newDocRef.id
+    newData.createdAt = systemDate
+    newData.updatedAt = systemDate
+    return newData
+  }
+
+  public async saveAll(userId: UserId, data: Task[]): Promise<Task[]> {
+    const result: Task[] = []
+
+    for (const task of data) {
+      const entity = toTaskEntity(task)
+      entity.createdAt = serverTimestamp()
+      entity.updatedAt = serverTimestamp()
+
+      const newDocRef = doc(this.getRef(userId))
+      await scope.set(newDocRef, entity)
+
+      const systemDate = new Date()
+      const newData = structuredClone(task)
+      newData.id = newDocRef.id
+      newData.createdAt = systemDate
+      newData.updatedAt = systemDate
+      result.push(newData)
+    }
+    return result
+  }
+
+  public async update(userId: UserId, data: Partial<Task>): Promise<Task> {
+    const docRef = doc(this.getRef(userId), data.id!)
+    const entity = toTaskEntity(data as Task)
+    entity.updatedAt = serverTimestamp()
+
+    const updateParams = Object.fromEntries(Object.entries(entity).filter(([, v]) => v !== undefined))
+
+    await scope.update(docRef, updateParams)
+
+    const newData = structuredClone(data)
+    newData.updatedAt = new Date()
+
+    return newData as Task
+  }
+
+  public async updateAll(userId: UserId, data: Partial<Task>[]): Promise<Task[]> {
+    const result: Task[] = []
+
+    for (const task of data) {
+      const entity = toTaskEntity(task as Task)
+      entity.updatedAt = serverTimestamp()
+
+      const updateParams = Object.fromEntries(Object.entries(entity).filter(([, v]) => v !== undefined))
+
+      const docRef = doc(this.getRef(userId), task.id!)
+      await scope.set(docRef, updateParams)
+
+      const newData = structuredClone(task)
+      newData.updatedAt = new Date()
+
+      result.push(newData as Task)
+    }
+
+    return result
+  }
+
+  public async delete(userId: UserId, taskIds: string[]): Promise<void> {
+    for (const taskId of taskIds) {
+      const docRef = doc(this.getRef(userId), taskId)
+      await scope.delete(docRef)
+    }
+  }
+
+  private convert(id: string, data: DocumentData): Task {
+    const task = { ...data, id } as Task
+    task.createdAt = data.createdAt?.toDate() ?? ''
+    task.updatedAt = data.createdAt?.toDate() ?? ''
+    return task
+  }
+
+  private async getRecords(q: Query): Promise<Task[]> {
+    return (await getDocs(q))
+      .docs.map(doc => {
+        return this.convert(doc.id, doc.data())
+      })
+  }
+}
