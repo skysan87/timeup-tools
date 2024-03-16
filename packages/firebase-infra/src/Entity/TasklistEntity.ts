981867e5cfd935d795204f8525f98472751c91a1
skysan <26142873+skysan87@users.noreply.github.com>
2024-03-16 18:04:19 +0900

    typecheckエラー修正

--- packages/firebase-infra/src/Entity/TasklistEntity.ts
+++ packages/firebase-infra/src/Entity/TasklistEntity.ts
@@ -2,8 +2,8 @@ import { UserId } from "@timeup-tools/core/value-object"
 import { FirebaseField } from "./common"
 
 export type TasklistEntity = FirebaseField & {
-  title: string
-  detail: string
+  title: string | null
+  detail: string | null
   userId: UserId
   maxIndex: number
   orderIndex: number
