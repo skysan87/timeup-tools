7b664b96b26ccc0610d71ef799be14bdfb06d623
skysan <26142873+skysan87@users.noreply.github.com>
2024-04-12 21:35:03 +0900

    グローバルメッセージを表示すると、タスクの入力フォームがずれるのを修正

--- apps/web/src/layouts/board-mobile.vue
+++ apps/web/src/layouts/board-mobile.vue
@@ -18,43 +18,59 @@ watch(
 </script>
 
 <template>
-  <!-- header -->
-  <div class="bg-gray-800 z-30">
-    <header class="container mx-auto text-white">
-      <div class="flex justify-between items-center fixed w-full left-0 bg-gray-800 px-2 h-10">
-        <h1 class="font-semibold text-xl leading-tight">
-          <span class="font-mono">{{ currentDate }}</span>
-        </h1>
-        <button class="outline-none" @click.left="switchMenu">
-          <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
-            <path v-show="!isMenuExpanded" d="M24 6h-24v-4h24v4zm0 4h-24v4h24v-4zm0 8h-24v4h24v-4z" />
-            <path v-show="isMenuExpanded"
-              d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z" />
-          </svg>
-        </button>
-      </div>
+  <div class="app-container">
+    <!-- header -->
+    <div class="z-30">
+      <header class="mx-auto text-white">
+        <div class="flex justify-between items-center bg-gray-800 px-2 h-10">
+          <h1 class="font-semibold text-xl leading-tight">
+            <span class="font-mono">{{ currentDate }}</span>
+          </h1>
+          <button class="outline-none" @click.left="switchMenu">
+            <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
+              <path v-show="!isMenuExpanded" d="M24 6h-24v-4h24v4zm0 4h-24v4h24v-4zm0 8h-24v4h24v-4z" />
+              <path v-show="isMenuExpanded"
+                d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z" />
+            </svg>
+          </button>
+        </div>
 
-      <div v-show="isMenuExpanded" class="fixed left-0 mt-10 pb-20 w-full bg-gray-800 h-full overflow-y-scroll">
-        <MenuCommand />
+        <div v-show="isMenuExpanded" class="fixed pb-20 w-full bg-gray-800 h-full overflow-y-scroll">
+          <MenuCommand />
 
-        <MenuSummary />
+          <MenuSummary />
 
-        <MenuProjects />
+          <MenuProjects />
 
-        <MenuHabits />
+          <MenuHabits />
 
-        <MenuConfig />
-      </div>
+          <MenuConfig />
+        </div>
 
-    </header>
-  </div>
+      </header>
+    </div>
 
-
-  <!-- contents -->
-  <div class="container mx-auto pt-10 h-screen overflow-hidden">
-    <div class="left-0 w-full bg-green-400 text-center">
+    <!-- message -->
+    <div class="text-center bg-green-400">
       {{ config.globalMessage }}
     </div>
-    <slot />
+
+    <!-- contents -->
+    <div class="h-full overflow-auto">
+      <slot />
+    </div>
   </div>
 </template>
+
+<style scoped>
+.app-container {
+  display: grid;
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+  grid-template:
+    "header" 40px
+    "message" min-content
+    "contents" auto;
+}
+</style>
